38ffed163ab6a1b2e08d50bb7d9d9d00b5f74c24
labwiki <labwiki@cs.umass.edu>
2013-03-08 17:25:08 -0500

    Add user's git repository

--- GIMIPortal/src/startLW.sh
+++ GIMIPortal/src/startLW.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 LWPORT=$1
+UNAME=$2
 
-LABWIKI_HOME=/home/johren/GIMI/GIMIPortal/src/omf_labwiki
-OMF_HOME=/home/johren/omf_web
+LABWIKI_HOME=/home/labwiki/GIMI/omf_labwiki
+OMF_HOME=/home/labwiki/omf_web
 
 source ~/.rvm/scripts/rvm
-#nohup ruby -I $LABWIKI_HOME/lib -I $OMF_HOME/lib $LABWIKI_HOME/lib/labwiki.rb --lw-config ~/norbit.yaml --port $LWPORT start > /dev/null &
-#nohup ruby $LABWIKI_HOME/bin/labwiki --lw-config ~/norbit.yaml --port $LWPORT start > /dev/null &
-nohup ruby -I $LABWIKI_HOME/lib -I $OMF_HOME/lib $LABWIKI_HOME/bin/labwiki --lw-config ~/norbit.yaml --port $LWPORT start > /dev/null &
+nohup ruby -I $LABWIKI_HOME/lib -I $OMF_HOME/lib $LABWIKI_HOME/bin/labwiki --lw-config /home/${UNAME}/exogeni.yaml --port $LWPORT start > /dev/null &
 
 lwpid=$!
 
--- GIMIPortal/src/startLabwiki.py
+++ GIMIPortal/src/startLabwiki.py
@@ -3,11 +3,14 @@ import lockfile
 import time
 import os
 import subprocess
+import sys
 
-myport = 5040 
+myport = 5020 
 endport = 5050 
 
 token = "/tmp/labwikitoken"
+
+uname = sys.argv[1]
 
 lock = lockfile.FileLock(token)
 with lock:
@@ -16,7 +19,7 @@ with lock:
         if os.path.exists(file_path):
             myport = myport + 1
         else:
-            pid = subprocess.call(["/home/johren/GIMI/GIMIPortal/src/startLW.sh", str(myport)])
+            pid = subprocess.call(["/home/labwiki/GIMI/GIMIPortal/src/startLW.sh", str(myport), uname])
 	    print myport
             time.sleep(3)
             break
