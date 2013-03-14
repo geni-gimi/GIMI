7767389e61332e1f9d3510af74d5c367970af07d
Gimi03 User <gimi03@pgeni.gpolab.bbn.com>
2013-03-13 20:51:54 -0400

    Cong's updates

--- omf_labwiki/test/omf_exec/omf_exec-norbit.sh
+++ omf_labwiki/test/omf_exec/omf_exec-norbit.sh
@@ -7,13 +7,12 @@ CWD=$(dirname $0)
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 rvm use ruby-1.8.7
 
-#ruby -I $OMF_HOME/omf-expctl/ruby -I $OMF_HOME/omf-common/ruby $OMF_HOME/omf-expctl/ruby/omf-expctl.rb \
-#  -C $CWD/etc/omf-expctl.norbit.yaml --log $CWD/etc/omf-expctl_log.xml $*
+#iget step1-ping_all.rb /home/cong/geniRenci/home/rods/
+
+ruby -I $OMF_HOME/omf-expctl/ruby -I $OMF_HOME/omf-common/ruby $OMF_HOME/omf-expctl/ruby/omf-expctl.rb \
+  -C /home/labwiki/GIMI/omf_labwiki/test/omf_exec/omf-expctl.yaml --no-am $*
 
 #ruby -r rubygems -I $OMF_HOME/omf-expctl/ruby -I $OMF_HOME/omf-common/ruby $OMF_HOME/omf-expctl/ruby/omf-expctl.rb \
 #     -C /etc/omf-expctl-5.4/omf-expctl.yaml -S gimic30 --log $CWD/etc/omf-expctl_log.xml $*
 
-#omf-5.4 exec --no-am -C /home/cong/work/labwiki/test/omf_exec/omf-expctl.yaml -e gimi-test-1 -S gimic31 $*
-
-ruby -r rubygems -I $OMF_HOME/omf-expctl/ruby -I $OMF_HOME/omf-common/ruby $OMF_HOME/omf-expctl/ruby/omf-expctl.rb \
-     -C $CWD/etc/omf-expctl.exogeni.yaml --log $CWD/etc/omf-expctl_log.xml $*
+#omf-5.4 exec --no-am -C /home/cong/work/labwiki/test/omf_exec/omf-expctl.yaml -e gimi-test-1 $*
