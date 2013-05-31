ae3e853a6f862cbb12924f113a5d00a91a1c1d4e
Keleigh O'Neil <koneil@bbn.com>
2013-05-31 12:39:55 -0400

    new makeTicket that takes in users and an expire time

--- gimiInit/iRODS.py
+++ gimiInit/iRODS.py
@@ -19,7 +19,6 @@ import simpleArtifact
 # 
 class iRODS:
     # This creates the iRODS experiment directory with initial XML files 
-    # and returns a ticket for read/write access to the directory
     def __init__(self, exp_id, exp_title, exp_first_name, exp_last_name, exp_org, slice_name):
         #set variables
         self.proj_id = "Project ID"
@@ -42,10 +41,7 @@ class iRODS:
         #create directories and include initial XML files
         self.makeDirectories()
         print 'iRODS setup was successful\n'
-        #make an initial ticket
-        myTicket=self.makeTicket()
-        print "Ticket for new directory: " + myTicket
-      
+             
 
     # Builds XML files and writes them to files
     def makeFiles(self):
@@ -79,16 +75,46 @@ class iRODS:
         subprocess.check_output(['iput', 'artifact.xml'])
         subprocess.check_output(['icd'])
       
+########## TICKETS ##########
 
     # Creates tickets for the experiment directory
-    def makeTicket(self):
+#    def makeTicket(self):
+#        subprocess.check_output(['icd'])
+#        ticket = subprocess.check_output(['iticket', 'create', 'write', self.exp_id])
+#        ticket = ticket.replace("ticket:","")
+#        ticket = ticket.replace("\n","")
+#        return ticket  
+ ##########   NEW CODE    #########     
+    # Creates tickets for the experiment directory taking in user restrictions & an expiration time
+    def makeTicket(self, users=None, expire_time=0):
         ticket = subprocess.check_output(['iticket', 'create', 'write', self.exp_id])
         ticket = ticket.replace("ticket:","")
         ticket = ticket.replace("\n","")
-        return ticket  
-      
+        if users is None:
+            users=[]
+        for x in users:
+            subprocess.check_output(['iticket', 'mod', ticket, 'add', 'user', x])
+        if expire_time != 0:
+            subprocess.check_output(['iticket', 'mod', ticket, 'expire', expire_time])
+        print "Ticket for new directory: " + ticket
+        return ticket 
 
+    # Creates tickets for the experiment directory taking in user restrictions & an expiration time
+    # expire_time must be UNIX timestamp
+    def extendTicket(self, ticket, expire_time=0):
+        if expire_time != 0:
+            subprocess.check_output(['iticket', 'mod', ticket, 'expire', expire_time])
+            print "Expiration date is now set to" + expire_time
+        return ticket 
+  ####################################
 
 # This creates an example iRODS object & creates the XML files & makes a ticket
-newiRODS = iRODS('exp_id14', 'exp_title', 'exp_first_name', 'exp_last_name', 'exp_org', "slice_name")
+newExp = iRODS('exp_id20', 'exp_title', 'exp_first_name', 'exp_last_name', 'exp_org', "slice_name")
+#make an initial ticket
+myTicket=newExp.makeTicket(['koneil2','koneil3'])
+
+myTicket=newExp.makeTicket(expire_time='1372654800')
+myTicket=newExp.makeTicket()
+
+
 
