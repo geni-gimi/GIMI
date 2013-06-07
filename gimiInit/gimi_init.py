f15d358f3c632ceb7479442f2c07a56144c588c4
Cong Wang <cwang@bua-1177.bbn.com>
2013-06-07 16:16:44 -0400

    modified gimi_init

--- gimiInit/gimi_init.py
+++ gimiInit/gimi_init.py
@@ -43,12 +43,17 @@ if (os.path.exists(workdirectory) == False):
             print ("Please answer Yes or No")
 
 #Get user information (experimenter name & organization)
+
+#TODO: get project name from urn
 experimenterName = raw_input("Tell us about yourself: enter your name: \n")
 
 exp_org= raw_input ("Your organization: \n")
 
 #Initialize iRODS
 print("Initializing iRODS")
+
+irodsenv = subprocess.check_output(['ienv'])
+print irodsenv
 
 os.system("iinit")
 
@@ -62,16 +67,19 @@ os.system("iinit")
 #else: 
 #    continue
 
+
 username = raw_input("Enter your GENI username: \n")
 
-os.system("omni.py listmyslices " + username)
+os.system("omni.py listmyslices ")
 
 slicename = raw_input("Please enter your preferred slice name for this experiment: \n")
 
 expName = raw_input("Enter your experiment name: \n")
 
 #Experiment ID = Experiment name + slice name + time (time stamp use time.time())
-expId = str(username) + "-" + str(expName) + "-" + str(time.strftime("%Y-%m-%dT%H-%M-%S",time.localtime(time.time())))
+expTime = str(time.strftime("%Y-%m-%dT%H:%M:%S",time.localtime(time.time())))
+expId = str(username) + "-" + str(expName) + "-" + expTime
+
 
 print ("derived experiment ID: " + expId)
 
@@ -79,27 +87,26 @@ print ("derived experiment ID: " + expId)
 print ("retrieving the manifest rspec...")
 
 manifestName="manifest-"+ expId + ".rspec"
-os.system("omni.py listresources -a pg-utah " + slicename + " -o --outputfile=" + workdirectory + "/"+manifestName)
+os.system("omni.py listresources " + slicename + " -a pg-utah -o --outputfile=" + workdirectory + "/"+manifestName)
         
 # This creates an example iRODS object & creates the XML files & makes a ticket
 
-newExp = iRODS.iRODS('proj_id', 'proj_title', 'PI_first_name', 'PI_last_name', 'PI_org', expId, expName, experimenterName, 'exp_last_name', exp_org, 'exp_start', 'exp_end', 'step_name', 'step_seq_id', 'resource_id', slicename, 'artifact_name')
-
-
+newRods = iRODS.iRODS('proj_authority', 'proj_name', 'proj_id', 'PI', 'proj_individual_authority', 'proj_individual_user', 'proj_date_time_type', 'proj_start', 'exp_authority', expName, expId, 'experimenter', exp_org, username, 'iso8601', expTime)
 #make an initial ticket
 #myTicket=newExp.makeTicket(['koneil2','koneil3'], '1372654800')
 
-myTicket=newExp.makeTicket(expire_time='1379654800')
+myTicket=newRods.makeTicket(expire_time='1379654800')
 #myTicket=newExp.makeTicket()
 
 while True:
     pushManifestOption = raw_input("Do you want to push manifest to iRODS? (Yes or No) \n")
  
     if (pushManifestOption in ("Yes", "Y", "yes", "y")):
-    #Push to iRODS
-        subprocess.check_output(['icd', expId])
-        subprocess.check_output(['iput', workdirectory + "/" + manifestName])
-        print ("Manefest " + manifestName + " has been pushed to iRODS")
+        #Push to iRODS
+        newRods.pushManifest(manifestName, workdirectory, slicename)
+#        subprocess.check_output(['icd', expId])
+#        subprocess.check_output(['iput', workdirectory + "/" + manifestName])
+#        print ("Manifest " + manifestName + " has been pushed to iRODS")
         break
  
     elif (pushManifestOption in ("No", "N", "no", "n")):
